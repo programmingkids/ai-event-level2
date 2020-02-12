72ad4382374718d2c8e5e7f7730447e6a4c918fa
Hiroshi Fujiwara <fujiwara@no1s.biz>
2020-02-12 15:26:08 +0900

    fix chapter04 for master

--- chapter04/work03.py
+++
@@ -1,6 +0,0 @@
-def double( number ):
-    print( number * 2 )
-
-
-double(3)
-double(6)
--- chapter04/work06.py
+++
@@ -1,7 +0,0 @@
-class Person:
-    def say_hello(self):
-        print("Hello")
-
-
-person = Person()
-person.say_hello()
